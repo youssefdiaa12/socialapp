8779cf8d2a83c22d990befe4d14d80a6ec9cc8f5
youssef diaa <youssefdiaa22@icloud.com>
2023-09-01 06:47:37 +0300

    creating app screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_media_ui/screens/search_screen.dart';
+
+import '../models/models.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/';
+
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Post> posts = Post.posts;
+
+  @override
+  Widget build(BuildContext context) {
+    Post? post = ModalRoute.of(context)!.settings.arguments as Post?;
+    if (post != null) posts.insert(0, post);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchScreen()));
+              },
+              style: TextButton.styleFrom(
+                fixedSize: const Size(100, 50),
+              ),
+              child: Text(
+                'For You',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                fixedSize: const Size(100, 50),
+              ),
+              child: Text(
+                'Following',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: const CustomBottomAppBar(),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.zero,
+              children: posts.map((post) {
+                return CustomVideoPlayer(post: post);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}
+
+
+
+
+
+
+
